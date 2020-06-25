bf872491e3b5d469667dc2a7776d3634ce958d50
Santiago Rodriguez <rodriguezsanticr@gmail.com>
2020-06-25 16:56:16 -0300

    agrega a.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
